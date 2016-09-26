2047b8c2f4b55478620c92f0ceb0611e608dd75c
Nhat Hoang <hoangnh092185@gmail.com>
2016-09-26 16:59:45 -0700

    add methods for monsters class and create RESTful functionality.

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -47,6 +47,14 @@ public class Person {
       return person;
     }
   }
+  public List<Monster> getMonsters() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM monsters where personId=:id";
+      return con.createQuery(sql)
+        .addParameter("id", this.id)
+        .executeAndFetch(Monster.class);
+    }
+  }
   @Override
  public boolean equals(Object otherPerson){
    if (!(otherPerson instanceof Person)) {
